64284609a5c85898f1ccafd4064e5fda45861ccd
RAJNI-KUMAWAT05 <rajnidadarwal5@gmail.com>
2026-06-04 18:51:14 +0530

    Refactor handleLogin to use dynamic API URL
    Update login redirection to use environment variable for API URL.

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -6,9 +6,9 @@ import bgImage from '../assets/signup-bg.jpg'; // Optional graphic illustration
 
 function Signup() {
 
-    const handleLogin = () => {
-        window.location.href = "http://localhost:3000/auth/google";
-    };
+   const handleLogin = () => {
+    window.location.href = `${import.meta.env.VITE_API_URL}/auth/google`;
+};
 
     return (
         <div
